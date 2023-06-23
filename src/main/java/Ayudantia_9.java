1f5dd4ecb893227b548e960e921c15134ff913ba
DiegoGM98 <132071916+DiegoGM98@users.noreply.github.com>
2023-06-22 23:45:12 -0400

    Agrego 3ra parte

---
+++ src/main/java/Ayudantia_9.java
@@ -0,0 +1,119 @@
+class Animal {
+    private String nombre;
+    private int edad;
+
+    public Animal(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
+
+class Perro extends Animal {
+    private String raza;
+
+    public Perro(String nombre, int edad, String raza) {
+        super(nombre, edad);
+        this.raza = raza;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+}
+
+class Gato extends Animal {
+    private String color;
+
+    public Gato(String nombre, int edad, String color) {
+        super(nombre, edad);
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
+
+class Automovil {
+    private String marca;
+    private String modelo;
+
+    public Automovil(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+}
+
+class SUV extends Automovil {
+    private int capacidadPasajeros;
+
+    public SUV(String marca, String modelo, int capacidadPasajeros) {
+        super(marca, modelo);
+        this.capacidadPasajeros = capacidadPasajeros;
+    }
+
+    public int getCapacidadPasajeros() {
+        return capacidadPasajeros;
+    }
+
+    public void setCapacidadPasajeros(int capacidadPasajeros) {
+        this.capacidadPasajeros = capacidadPasajeros;
+    }
+}
+
+class Camioneta extends Automovil {
+    private int capacidadCarga;
+
+    public Camioneta(String marca, String modelo, int capacidadCarga) {
+        super(marca, modelo);
+        this.capacidadCarga = capacidadCarga;
+    }
+
+    public int getCapacidadCarga() {
+        return capacidadCarga;
+    }
+
+    public void setCapacidadCarga(int capacidadCarga) {
+        this.capacidadCarga = capacidadCarga;
+    }
+}
